40435b47572234e5ccb214c58f1bd2ca2724cace
Isha Madlani <ishamadlani824@gmail.com>
2026-07-16 06:30:55 -0600

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st=""
+        for ind,i in enumerate(s):
+            if i.isalpha() or i.isdigit():
+                st += i.lower()
+        l=len(s)//2
+        r=-1
+        print (st)
+        if not st:
+            
+            return True
+        for i in range(l):
+       
+            if st[i]==st[r]:
+                r-=1
+            else:
+                return False
+
+       
+        return True
